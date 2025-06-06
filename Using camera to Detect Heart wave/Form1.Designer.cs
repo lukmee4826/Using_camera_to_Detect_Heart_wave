bd2ee76fd4efaf09e12e894a3b62be35bd56d4ce
lukmee4826 <lukmeepook@gmail.com>
2025-06-06 11:27:39 +0900

    Can use camera by openCV sharp and have a button to Activate and Deactivate

--- Using camera to Detect Heart wave/Form1.Designer.cs
+++ Using camera to Detect Heart wave/Form1.Designer.cs
@@ -28,13 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ActiveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(670, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // ActiveButton
+            // 
+            this.ActiveButton.BackColor = System.Drawing.Color.SeaGreen;
+            this.ActiveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ActiveButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ActiveButton.Location = new System.Drawing.Point(704, 199);
+            this.ActiveButton.Name = "ActiveButton";
+            this.ActiveButton.Size = new System.Drawing.Size(75, 45);
+            this.ActiveButton.TabIndex = 1;
+            this.ActiveButton.Text = "Start";
+            this.ActiveButton.UseVisualStyleBackColor = false;
+            this.ActiveButton.Click += new System.EventHandler(this.ActiveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ActiveButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ActiveButton;
     }
 }
 
--- Using camera to Detect Heart wave/Form1.cs
+++ Using camera to Detect Heart wave/Form1.cs
@@ -7,14 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
+using System.Threading;
+
+
 
 namespace Using_camera_to_Detect_Heart_wave
 {
     public partial class Form1 : Form
     {
+        VideoCapture capture = new VideoCapture(0); // 0 for the default camera
+        Mat image = new Mat();
         public Form1()
         {
             InitializeComponent();
+        }
+        Thread thread;
+
+        bool isActive = false;
+        private void ActiveButton_Click(object sender, EventArgs e)
+        {
+            if (!isActive)
+            {
+                
+                ActiveButton.Text = "Stop";
+                ActiveButton.BackColor = Color.Red;
+                ActiveButton.ForeColor = Color.White;
+                thread = new Thread(new ThreadStart(ShowCamera));
+                thread.Start();
+                isActive = true;
+            }
+            else
+            {
+                ActiveButton.Text = "Start";
+                ActiveButton.BackColor = Color.Green;
+                ActiveButton.ForeColor = Color.White;
+                thread.Abort();
+                thread.Join(); // Ensure the thread is properly terminated
+                pictureBox1.Image = null; // Clear the picture box
+                isActive = false;
+            }
+        }
+
+        public void ShowCamera()
+        {
+            while (true)
+            {
+                capture.Read(image);
+                if (image.Empty())
+                {
+                    break;
+                }
+                pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
+            }
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
