94e8d51b1a87cfd4b202cfefa0f41b7f48554832
lukmee4826 <lukmeepook@gmail.com>
2025-06-06 13:07:18 +0900

    Now it can detect nose, center of nose and can save rgb the center of nose into csv format

--- Using camera to Detect Heart wave/Form1.Designer.cs
+++ Using camera to Detect Heart wave/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.ActiveButton = new System.Windows.Forms.Button();
+            this.save_data = new System.Windows.Forms.Button();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,7 +49,7 @@
             this.ActiveButton.BackColor = System.Drawing.Color.SeaGreen;
             this.ActiveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ActiveButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ActiveButton.Location = new System.Drawing.Point(704, 199);
+            this.ActiveButton.Location = new System.Drawing.Point(700, 181);
             this.ActiveButton.Name = "ActiveButton";
             this.ActiveButton.Size = new System.Drawing.Size(75, 45);
             this.ActiveButton.TabIndex = 1;
@@ -55,11 +57,28 @@
             this.ActiveButton.UseVisualStyleBackColor = false;
             this.ActiveButton.Click += new System.EventHandler(this.ActiveButton_Click);
             // 
+            // save_data
+            // 
+            this.save_data.Location = new System.Drawing.Point(700, 250);
+            this.save_data.Name = "save_data";
+            this.save_data.Size = new System.Drawing.Size(75, 42);
+            this.save_data.TabIndex = 2;
+            this.save_data.Text = "Save";
+            this.save_data.UseVisualStyleBackColor = true;
+            this.save_data.Click += new System.EventHandler(this.save_data_Click);
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.save_data);
             this.Controls.Add(this.ActiveButton);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -73,6 +92,8 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button ActiveButton;
+        private System.Windows.Forms.Button save_data;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
     }
 }
 
--- Using camera to Detect Heart wave/Form1.cs
+++ Using camera to Detect Heart wave/Form1.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using OpenCvSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using OpenCvSharp;
-using System.Threading;
 
 
 
@@ -16,7 +17,9 @@ namespace Using_camera_to_Detect_Heart_wave
 {
     public partial class Form1 : Form
     {
-        VideoCapture capture = new VideoCapture(0); // 0 for the default camera
+
+        private List<string> waveDataLog = new List<string>();
+        VideoCapture capture = new VideoCapture(0);
         Mat image = new Mat();
         public Form1()
         {
@@ -43,22 +46,111 @@ namespace Using_camera_to_Detect_Heart_wave
                 ActiveButton.BackColor = Color.Green;
                 ActiveButton.ForeColor = Color.White;
                 thread.Abort();
-                thread.Join(); // Ensure the thread is properly terminated
-                pictureBox1.Image = null; // Clear the picture box
+                thread.Join(); 
+                pictureBox1.Image = null; 
                 isActive = false;
             }
         }
 
         public void ShowCamera()
         {
+            
+            CascadeClassifier faceCascade = new CascadeClassifier("C:\\code\\Iwate Internship\\Using camera to Detect Heart wave\\Using camera to Detect Heart wave\\Feature\\nose.xml");
+
             while (true)
             {
                 capture.Read(image);
-                if (image.Empty())
+                if (image.Empty()) break;
+
+                // Convert to grayscale for detection
+                Mat grayImage = new Mat();
+                Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGRA2GRAY);
+
+                // Detect faces/noses
+                Rect[] faces = faceCascade.DetectMultiScale(grayImage, 1.1, 4, HaarDetectionTypes.ScaleImage);
+
+                if (faces.Length > 0)
                 {
-                    break;
+                    Rect face = faces[0];
+                    Cv2.Rectangle(image, face, Scalar.Green, 2);
+
+                    int centerX = face.X + face.Width / 2;
+                    int centerY = face.Y + face.Height / 2;
+
+                    Vec3b pixelColor = image.At<Vec3b>(centerY, centerX);
+                    byte blue = pixelColor.Item0;
+                    byte green = pixelColor.Item1;
+                    byte red = pixelColor.Item2;
+
+                    // Save data to list if saving is active
+                    if (isSave)
+                    {
+                        string dataLine = $"{DateTime.Now:HH:mm:ss.fff},{red},{green},{blue}";
+                        waveDataLog.Add(dataLine);
+                    }
+
+                    this.Invoke(new Action(() =>
+                    {
+                        this.Text = $"Nose RGB: R={red} G={green} B={blue}";
+                    }));
+
+                    Cv2.Circle(image, new OpenCvSharp.Point(centerX, centerY), 2, Scalar.Red, -1);
                 }
+
+                pictureBox1.Image?.Dispose();
                 pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
+
+                Cv2.WaitKey(1);
+            }
+        }
+
+
+
+        bool isSave = false;
+        private void save_data_Click(object sender, EventArgs e)
+        {
+            if (isSave)
+            {
+                // Save to CSV
+                if (waveDataLog.Count > 0)
+                {
+                    using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                    {
+                        saveFileDialog.Filter = "CSV files (*.csv)|*.csv";
+                        saveFileDialog.Title = "Save RGB Data";
+                        saveFileDialog.FileName = "wave_data.csv";
+
+                        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                            {
+                                writer.WriteLine("Time,R,G,B");
+                                foreach (var line in waveDataLog)
+                                {
+                                    writer.WriteLine(line);
+                                }
+                            }
+
+                            MessageBox.Show("Wave data saved successfully!");
+                        }
+                    }
+
+                    waveDataLog.Clear(); // clear after save
+                }
+
+                
+                save_data.Text = "Save";
+                save_data.BackColor = Color.White;
+                save_data.ForeColor = Color.Black;
+                isSave = false;
+            }
+            else
+            {
+                
+                save_data.Text = "Saving..";
+                save_data.BackColor = Color.Green;
+                save_data.ForeColor = Color.White;
+                isSave = true;
             }
         }
 
